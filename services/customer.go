8ffd4898f29f85e159f1653577547f7703a582b0
kingcrusher <tiwarivikrant9764@gmail.com>
2024-10-31 15:06:51 +0530

    made post customer to upsert instead of insert

--- services/customer.go
+++ services/customer.go
@@ -3,20 +3,26 @@ package services
 import (
 	"github.com/RPJ-Overseas-Exim/yourpharma-htmx/db/models"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type CustomerService struct {
-    dbConn *gorm.DB
+	dbConn *gorm.DB
 }
 
 func (cs *CustomerService) PostCustomer(id, name, email string, number *int, address *string) error {
-    customer := models.NewCustomer(id, name, email , number , address )
-    err := cs.dbConn.Create(&customer).Error
-    return err
+	customer := models.NewCustomer(id, name, email, number, address)
+	err := cs.dbConn.
+		Clauses(clause.OnConflict{
+			Columns:   []clause.Column{{Name: "id"}},
+			DoUpdates: clause.Assignments(map[string]interface{}{"email": email}),
+		}).
+	Create(&customer).Error
+	return err
 }
 
-func NewCustomerService(dbConn *gorm.DB) *CustomerService{
-    return &CustomerService{
-        dbConn,
-    }
+func NewCustomerService(dbConn *gorm.DB) *CustomerService {
+	return &CustomerService{
+		dbConn,
+	}
 }
